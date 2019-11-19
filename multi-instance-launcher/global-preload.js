989cf7fc0dd9be35180044eafbe24bb3cab4798e
Nicholas Goodman <nicholasdgoodman@users.noreply.github.com>
2019-11-19 09:44:34 -0600

    Second fix to same bug

--- multi-instance-launcher/global-preload.js
+++ multi-instance-launcher/global-preload.js
@@ -141,7 +141,7 @@ async function fdc3ProxyWindowPreload() {
 
     fdc3.addContextListener(ctx => {
         console.log('fdc3 context listener');
-        if(fdc3ProxyChannel.connections.length > 0 && evt.identity.name === currentWindow.identity.name) {
+        if(fdc3ProxyChannel.connections.length > 0) {
             fdc3ProxyChannel.dispatch(
                 fdc3ProxyChannel.connections[0],
                 'context-changed',
@@ -151,7 +151,7 @@ async function fdc3ProxyWindowPreload() {
 
     fdc3.addEventListener('channel-changed', evt => {
         console.log('fdc3 channel changed');
-        if(fdc3ProxyChannel.connections.length > 0) {
+        if(fdc3ProxyChannel.connections.length > 0 && evt.identity.name === currentWindow.identity.name) {
             fdc3ProxyChannel.dispatch(
                 fdc3ProxyChannel.connections[0],
                 'channel-changed',
