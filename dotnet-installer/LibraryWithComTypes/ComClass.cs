0ab228375ba7b8ba34736e04d84a71af97883c60
Nicholas Goodman <nicholas@openfin.co>
2020-12-04 09:56:40 -0500

    Added simple installer utility demo for deploying COM-registered .NET DLLs

---
+++ dotnet-installer/LibraryWithComTypes/ComClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace LibraryWithComTypes
+{
+    [Guid("F9FCABF1-EAD6-468E-B45A-5116A09435F4"), ComVisible(true)]
+    public interface ComInterface
+    {
+    }
+
+    [Guid("C918F5A5-F24F-4D05-98E4-6C9DB8EAC529"), ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class ComClass : ComInterface
+    {
+    }
+}
